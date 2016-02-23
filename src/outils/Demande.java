617702d2fd29414313d464493883c495c272678e
Cédric CANESTRARI <canestrari.cedric@gmail.com>
2016-02-23 14:42:39 +0100

    Création

---
+++ src/outils/Demande.java
@@ -0,0 +1,9 @@
+package outils;
+
+public class Demande {
+	public Demande() {}
+	public Demande(int etage, Sens montee) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/outils/Sens.java
@@ -0,0 +1,7 @@
+package outils;
+
+public enum Sens {
+	DESCENTE,
+	MONTEE,
+	INDEFINI;
+}
