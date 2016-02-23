1849af87c0f7a8d2788ff6e0de92516bb38db0f9
Baptiste Charrier <baptistech@hotmail.fr>
2016-02-23 16:06:12 +0100

    Début test Demande

--- src/outils/Demande.java
+++ src/outils/Demande.java
@@ -1,9 +1,24 @@
 package outils;
 
 public class Demande {
-	public Demande() {}
-	public Demande(int etage, Sens montee) {
-		// TODO Auto-generated constructor stub
+	private Integer numEtage;
+	private Sens sens;
+	public Demande() {
+		numEtage = null;
 	}
+	public Demande(int etage, Sens montee) {
+		numEtage = etage;
+		sens = montee;
+	}
+	public int etage() { return numEtage; }
 
+	public boolean estIndefini() {
+		return (this.numEtage == null ? true : false);
+	}
+	public boolean enMontee() {
+		return (Sens.MONTEE == sens) ? true : false;
+	}
+	public boolean enDescente() {
+		return (Sens.DESCENTE == sens) ? true : false;
+	}
 }
