ffdac5dec2004e7c2922031dc6f9112f635d546d
LesageMarjorie <marjorie.lesage@etu.univ-amu.fr>
2016-02-28 13:38:06 +0100

    Commentaires de la classe Demande

--- src/outils/Demande.java
+++ src/outils/Demande.java
@@ -1,23 +1,60 @@
 package outils;
 
+/**
+ * Une demande représente une demande faite par un utilisateur, interne ou externe à l'ascenseur
+ * @author marjorie
+ *
+ */
 public class Demande {
+	/**
+	 * Le numéro d'étage
+	 */
 	private Integer numEtage;
+	/**
+	 * Le sens de monté
+	 */
 	private Sens sens;
+	
+	/**
+	 * Constructeur vide de Demande
+	 */
 	public Demande() {
 		numEtage = null;
 	}
+	
+	/**
+	 * Constructeur de Demande avec deux paramètres, représente une demande à un étage dans un sens
+	 * @param etage, le numéro de l'étage
+	 * @param montee, le sens de monté
+	 */
 	public Demande(int etage, Sens montee) {
 		numEtage = etage;
 		sens = montee;
 	}
+	/**
+	 * Renvoi le numéro de l'étage
+	 * @return un int représentant le numéro de l'étage
+	 */
 	public int etage() { return numEtage; }
 
+	/**
+	 * Retourne vrai si le numéro de l'étage n'est pas définit et faux sinon
+	 * @return true si le numéro d'étage est null et false sinon
+	 */
 	public boolean estIndefini() {
 		return (this.numEtage == null ? true : false);
 	}
+	/**
+	 * Retourne vrai si le sens est en monté et faux sinon
+	 * @return true si le sens est en monté et faux sinon
+	 */
 	public boolean enMontee() {
 		return (Sens.MONTEE == sens) ? true : false;
 	}
+	/**
+	 * Retourne vrai si le sens est en déscente et faux sinon
+	 * @return true si le sens est en déscente et faux sinon
+	 */
 	public boolean enDescente() {
 		return (Sens.DESCENTE == sens) ? true : false;
 	}
