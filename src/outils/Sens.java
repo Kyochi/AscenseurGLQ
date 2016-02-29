da6513e1ea3a4ac43a0ec9ada7c42f8d4e5c0c89
LesageMarjorie <marjorie.lesage@etu.univ-amu.fr>
2016-02-29 17:23:15 +0100

    Ajout toString() classe Sens

--- src/outils/Sens.java
+++ src/outils/Sens.java
@@ -2,11 +2,26 @@ package outils;
 
 /**
  * Sens représente une énumération des différents sens possible
+ * 
  * @author marjorie
  *
  */
 public enum Sens {
-	DESCENTE,
-	MONTEE,
-	INDEFINI;
+	DESCENTE, MONTEE, INDEFINI;
+
+	/**
+	 * Méthode surcharger de toString
+	 * @return un string qui est le sens
+	 */
+	public String toString() {
+		switch (this) {
+		case MONTEE:
+			return "^";
+		case DESCENTE:
+			return "v";
+		default:
+			return "-";
+
+		}
+	}
 }
