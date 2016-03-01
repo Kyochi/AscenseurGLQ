1f2c90d51024855caee9a7301c7021ef5771c9bb
LesageMarjorie <l14024024@C-CA-S111-05.salsa.univ-amu.fr>
2016-03-01 15:34:20 +0100

    Controleur

--- src/outils/Controleur.java
+++ src/outils/Controleur.java
@@ -2,4 +2,46 @@ package outils;
 
 public class Controleur implements IControleur{
 
+	@Override
+	public void MAJSens() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stocker(Demande d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void MAJPosition() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viderStock() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eteindreTousBoutons() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void interrogerStock() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enleverDuStock(Demande d) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/outils/IControleur.java
+++ src/outils/IControleur.java
@@ -1,5 +1,35 @@
 package outils;
 
 public interface IControleur {
-
+	/**
+	 * met sens à INDEFINI si la cabine est arrêtée, MONTEE si la cabine monte et DESCENTE si elle descend
+	 */
+	void MAJSens();
+	/**
+	 * stocke la demande
+	 * @param d représente une Demande
+	 */
+	void stocker(Demande d);
+	/**
+	 * incrémente position de 1 si sens vaut MONTEE, décrémente de 1 si sens vaut DESCENTE
+	 */
+	void MAJPosition();
+	/**
+	 * vide le stock des demandes
+	 */
+	void viderStock();
+	/**
+	 * éteint tous les boutons
+	 */
+	void eteindreTousBoutons();
+	/**
+	 * renvoie la demande du stock qui vérifie certaines conditions par rapport à la position et au sens ou au sensPrecedent
+	 */
+	void interrogerStock();
+	/**
+	 * enlève la demande du stock
+	 * @param d représente une Demande
+	 */
+	void enleverDuStock(Demande d);
+	
 }
