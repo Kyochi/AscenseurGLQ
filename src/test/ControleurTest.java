117577a5712d4546ba4ea01e251e060511ddd165
Lesage <lesage.marjorie.18@gmail.com>
2016-03-12 23:36:46 +0100

    Test 1 passé et automatisé

--- src/test/ControleurTest.java
+++ src/test/ControleurTest.java
@@ -2,7 +2,13 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import outils.Controleur;
@@ -10,6 +16,42 @@ import outils.Demande;
 import outils.Sens;
 
 public class ControleurTest {
+
+	private static OutputStream sortie; // flux de redirection de la sortie
+	private static PrintStream original; // flux de sortie initial
+	private static String lineSeparator; // saut de ligne
+	private static int indiceAffichage; // variable utilisee par la methode
+	private static String signalPal = "signal de franchissement de palier";// dernierAffichage
+	private static String arretPEtg = "arreter prochain étage";
+	private static String monter = "monter";
+	private static String desc = "descendre";
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		original = System.out;
+		sortie = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(sortie));
+		lineSeparator = System.lineSeparator();
+		indiceAffichage = 0;
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.setOut(original);
+		original = null;
+		sortie = null;
+	}
+
+	/*
+	 * Renvoie le texte qui vient d'etre affiche sur la sortie depuis le dernier
+	 * appel de la methode
+	 */
+	protected String dernierAffichage() {
+		String resultat = sortie.toString().substring(indiceAffichage);
+		indiceAffichage = sortie.toString().length();
+		return resultat;
+	}
+	
 	private Demande d1,d2;
 	private Controleur ctrl;
 	
@@ -29,7 +71,22 @@ public class ControleurTest {
 		assertSame(true,ctrl.getListeDemande().estVide());
 		ctrl.demander(d2);
 		assertSame(true,ctrl.getListeDemande().estVide());
-		
+		assertEquals(
+				"appel 1^" + lineSeparator + 
+				"allumer bouton 1^" + lineSeparator + 
+				desc + lineSeparator + 
+				arretPEtg + lineSeparator + 
+				signalPal + lineSeparator + 
+				"eteindre bouton 1^" + lineSeparator + 
+				"appel 4-" + lineSeparator + 
+				"allumer bouton 4-" + lineSeparator + 
+				monter + lineSeparator + 
+				signalPal + lineSeparator + 
+				signalPal + lineSeparator + 
+				arretPEtg + lineSeparator + 
+				signalPal + lineSeparator + 
+				"eteindre bouton 4^" + lineSeparator
+				,dernierAffichage()
+		);
 	}
-
 }
