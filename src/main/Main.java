cf69c2008406fa30b6c4bc01c5e9954ea52afc16
LesageMarjorie <l14024024@C-CA-S111-05.salsa.univ-amu.fr>
2016-03-22 13:59:09 +0100

    TP5

---
+++ src/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import outils.Demande;
+import operative.Cabine;
+import operative.IUG;
+import operative.ICabine;
+import operative.IIUG;
+import commande.IControleur;
+import commande.Controleur;
+import commande.IListeTrieeCirculaire;
+import commande.ListeTrieeCirculaireDeDemandes;
+
+public class Main {
+    public static void main(String[] args) {
+    	try { 
+	    	int nbEtages = 7; // nombre d'étages
+	        int hauteurEtage = 3; // hauteur d'un étage en nombre de pas
+	        IIUG iug = new IUG( nbEtages);
+	        ICabine cabine = new Cabine( 
+	        		500, // délai d'un pas
+	        		hauteurEtage, // hauteur d'un étage en nombre de pas
+	        		4); // délai de l'arrêt cabine
+	        IListeTrieeCirculaire<Demande> stock = new ListeTrieeCirculaireDeDemandes( nbEtages);
+	        IControleur controleur = new Controleur( nbEtages, iug, cabine, stock);
+	        cabine.assignerControleur( controleur);
+	        iug.assignerControleur( controleur);
+	    } catch (Exception e) { 
+	    	System.out.println(e.toString()); 
+	    }
+    }
+}
